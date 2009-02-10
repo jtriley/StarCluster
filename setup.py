64df6d72e28bfb695e182a534db8351f830f1765
Justin Riley <justin.t.riley@gmail.com>
2009-02-10 18:31:27 -0500

    added python eggs to gitignore set proper project name in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(
-    name = 'python-cuda',
+    name = 'molsim-cluster',
     version = '0.0.1',
     packages = ['molsim'],
     package_dir = {'cuda':'cuda'},
