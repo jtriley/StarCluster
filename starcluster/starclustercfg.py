fa798e15bda822d68a00f4ede40243d6e25c73f2
Justin Riley <jtriley@mit.edu>
2009-06-18 12:23:26 -0400

    reworked starclustercfg.py. config is now loaded on import without warnings/errors; even if config is completely invalid. added methods is_valid and validate_or_exit for checking configuration errors the above allows a developer to trigger config validation when they see fit added validate_or_exit() config check to manage-cluster.py

--- starcluster/starclustercfg.py
+++ starcluster/starclustercfg.py
@@ -9,124 +9,152 @@ from templates.config import config_template
 from starcluster import EC2
 
 """
-Reads all variables defined in .starclustercfg config file into starclustercfg module's namespace
+Reads starcluster configuration settings defined in ~/.starclustercfg config file into starclustercfg module's namespace
 """
 
 log = logging.getLogger('starcluster')
 
-class StarClusterCfg:
-    instance_types = {
-        'm1.small': True,
-        'm1.large': True,
-        'm1.xlarge': True,
-        'c1.medium': True,
-        'c1.xlarge': True,
-    }
 
-    def __init__(self):
-        # TODO: Make this better still...this at least gets the job done for now
-        if not os.path.exists(os.path.expanduser('~/.starclustercfg')):
-            print '>>> please create ~/.starclustercfg...template is below'
-            print config_template
-            sys.exit(1)
+instance_types = {
+    'm1.small': True,
+    'm1.large': True,
+    'm1.xlarge': True,
+    'c1.medium': True,
+    'c1.xlarge': True,
+}
 
-        self.config = ConfigParser.ConfigParser()
-        self.config.read(os.path.expanduser('~/.starclustercfg'))
+def _get_int(config, section, option):
+    try:
+        opt = config.getint(section,option)
+    except (NoOptionError):
+        opt = None
+    return opt
 
-        ec2_options = [
-            ('AWS_ACCESS_KEY_ID',self.get_string),
-            ('AWS_SECRET_ACCESS_KEY', self.get_string),
-            ('AWS_USERID', self.get_string),
-            ('KEYNAME', self.get_string),
-            ('KEY_LOCATION', self.get_string),
-        ]
+def _get_string(config, section, option):
+    try:
+        opt = config.get(section,option)
+    except (NoOptionError):
+        opt = None
+    return opt
 
-        starcluster_options = [
-            ('MASTER_IMAGE_ID', self.get_string),
-            ('IMAGE_ID', self.get_string),
-            ('INSTANCE_TYPE', self.get_string),
-            ('AVAILABILITY_ZONE', self.get_string),
-            ('ATTACH_VOLUME', self.get_string),
-            ('VOLUME_DEVICE', self.get_string),
-            ('VOLUME_PARTITION', self.get_string),
-            ('DEFAULT_CLUSTER_SIZE', self.get_int),
-            ('CLUSTER_USER', self.get_string)
-        ]
+ec2_options = [
+    ('AWS_ACCESS_KEY_ID',_get_string, True),
+    ('AWS_SECRET_ACCESS_KEY', _get_string, True),
+    ('AWS_USERID', _get_string, True),
+    ('KEYNAME', _get_string, True),
+    ('KEY_LOCATION', _get_string, True),
+]
 
-        section = "section ec2"
-        for opt in ec2_options:
-            globals()[opt[0]] = opt[1](section, opt[0])
+starcluster_options = [
+    ('MASTER_IMAGE_ID', _get_string, False),
+    ('IMAGE_ID', _get_string, True),
+    ('INSTANCE_TYPE', _get_string, False),
+    ('AVAILABILITY_ZONE', _get_string, False),
+    ('ATTACH_VOLUME', _get_string, False),
+    ('VOLUME_DEVICE', _get_string, False),
+    ('VOLUME_PARTITION', _get_string, False),
+    ('DEFAULT_CLUSTER_SIZE', _get_int, True),
+    ('CLUSTER_USER', _get_string, False)
+]
 
-        section = "section starcluster"
-        for opt in starcluster_options:
-            globals()[opt[0]] = opt[1](section, opt[0])
+sections = [
+    ("section ec2", ec2_options),
+    ("section starcluster", starcluster_options)
+]
 
-        if not self.validate():
-            log.error('configuration error...exiting')
-            sys.exit(1)
+def load_settings():
+    # TODO: create the template file for them
+    if not os.path.exists(os.path.expanduser('~/.starclustercfg')):
+        print '>>> It appears this is your first time using StarCluster.'
+        print '>>> Please create $HOME/.starclustercfg using the template below:'
+        print config_template
+        sys.exit(1)
 
-    def validate(self):
-        #todo check for network connectivity
-        conn = EC2.AWSAuthConnection(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
-        error = conn.describe_instances().is_error
-        if error:
-            log.error('>>> Invalid AWS_ACCESS_KEY_ID/AWS_SECRET_ACCESS_KEY combination')
-            return False
+    config = ConfigParser.ConfigParser()
+    config.read(os.path.expanduser('~/.starclustercfg'))
+
+    for section in sections:
+        section_name = section[0]; section_opts = section[1]
+        for opt in section_opts:
+            name = opt[0]; func = opt[1]; required = opt[2]
+            value = func(config,section_name, name)
+            globals()[name] = value
+
+def is_valid():
+    if not _has_all_required_settings():
+        log.error('Please specify the required settings in ~/.starclustercfg')
+        return False
+
+    #todo check for network connectivity
+    if not _has_valid_credentials():
+        log.error('Invalid AWS_ACCESS_KEY_ID/AWS_SECRET_ACCESS_KEY combination')
+        return False
+
+    if not _has_keypair(KEYNAME):
+        log.error('Could not find KEYNAME = %s. Please check your settings' % KEYNAME)
+        return False
     
-        keypairs = conn.describe_keypairs().parse()
-        has_keyname = False
-        for key in keypairs:
-            if key[1] == KEYNAME:
-                has_keyname = True
+    if not os.path.exists(KEY_LOCATION):
+        log.error('KEY_LOCATION=%s does not exist' % KEY_LOCATION)
+        return False
 
-        if not has_keyname:
-            log.error('>>> KEYNAME = %s not found' % KEYNAME)
-            return False
-        
-        if not os.path.exists(KEY_LOCATION):
-            log.error('>>> KEY_LOCATION=%s does not exist' % KEY_LOCATION)
-            return False
+    if IMAGE_ID is None:
+        log.error('You did not specify IMAGE_ID')
+        return False
 
-        if IMAGE_ID is None:
-            log.error('You did not specify IMAGE_ID')
-            return False
+    if not instance_types.has_key(INSTANCE_TYPE):
+        log.error('Missing or invalid INSTANCE_TYPE')
+        return False
+    
+    if DEFAULT_CLUSTER_SIZE is None:
+        log.error('>>> Required option DEFAULT_CLUSTER_SIZE missing from ~/.starclustercfg')
+        return False
 
-        if not self.instance_types.has_key(INSTANCE_TYPE):
-            log.error('Missing or invalid INSTANCE_TYPE')
-            return False
-        
-        if DEFAULT_CLUSTER_SIZE is None:
-            log.error('>>> Required option DEFAULT_CLUSTER_SIZE missing from ~/.starclustercfg')
-            return False
+    if DEFAULT_CLUSTER_SIZE < 0:
+        log.error('>>> DEFAULT_CLUSTER_SIZE must be a positive integer')
+        return False
+    
+    if CLUSTER_USER is None:
+        log.warn('>>> No CLUSTER_USER specified. Defaulting to sgeadmin user')
+        globals()['CLUSTER_USER'] = 'sgeadmin'
 
-        if DEFAULT_CLUSTER_SIZE < 0:
-            log.error('>>> DEFAULT_CLUSTER_SIZE must be a positive integer')
-            return False
-        
-        if CLUSTER_USER is None:
-            log.warn('>>> No CLUSTER_USER specified. Defaulting to sgeadmin user')
-            globals()['CLUSTER_USER'] = 'sgeadmin'
+    return True
 
-        return True
-        
-        
-    def load_everything(self):
-        for section in self.config.sections():
-            for option in self.config.options(section):
-                globals()[option.upper()] = self.config.get(section,option)
+def _has_all_required_settings():
+    has_all_required = True
+    for section in sections:
+        section_name = section[0]; section_opts = section[1]
+        for opt in section_opts:
+            name = opt[0]; required = opt[2]
+            if required and globals()[name] is None:
+                log.warn('Missing setting %s under section "%s"' % (name,section_name))
+                has_all_required = False
+    return has_all_required
 
-    def get_int(self, section,option):
-        try:
-            opt = self.config.getint(section,option)
-        except (NoOptionError):
-            opt = None
-        return opt
+def validate_or_exit():
+    if not is_valid():
+        log.error('configuration error...exiting')
+        sys.exit(1)
 
-    def get_string(self, section, option):
-        try:
-            opt = self.config.get(section,option)
-        except (NoOptionError):
-            opt = None
-        return opt
+def _get_conn():  
+    return EC2.AWSAuthConnection(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
 
-StarClusterCfg()
+def _has_valid_credentials():
+    conn = _get_conn()
+    return not conn.describe_instances().is_error
+
+def _has_keypair(keyname):
+    conn = _get_conn()
+    keypairs = conn.describe_keypairs().parse()
+    has_keypair = False
+    for key in keypairs:
+        if key[1] == KEYNAME:
+            has_keypair = True
+    return has_keypair
+    
+def _load_everything(config):
+    for section in config.sections():
+        for option in config.options(section):
+            globals()[option.upper()] = config.get(section,option)
+
+load_settings()
