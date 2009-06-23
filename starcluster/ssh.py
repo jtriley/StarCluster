79ac59878768bd71b884d29ea6a8f90c2f257501
Justin Riley <justin.t.riley@gmail.com>
2009-06-23 01:52:25 -0400

    added interactive shell code to ssh.Connection that could possibly eliminate the need to run os.system('ssh...') (not quite ready for stable use) moved cfg.validate_or_exit(), which validates ALL options, to only occur on -s/-x options for performance reasons for other -l, -m, -t, etc options, only a small subset of the options are needed, and thus a number of checks can be skipped which saves time

--- starcluster/ssh.py
+++ starcluster/ssh.py
@@ -11,6 +11,16 @@ import tempfile
 import paramiko
 import logging
 
+import socket
+import sys
+
+# windows does not have termios...
+try:
+    import termios
+    import tty
+    HAS_TERMIOS = True
+except ImportError:
+    HAS_TERMIOS = False
 
 log = logging.getLogger('starcluster')
 
@@ -47,9 +57,9 @@ class Connection(object):
             pkey = None
             if private_key:
                 log.debug('private key specified')
-                if private_key.endswith('rsa'):
+                if private_key.endswith('rsa') or private_key.count('rsa'):
                     pkey = self._load_rsa_key(private_key, private_key_pass)
-                elif private_key.endswith('dsa'):
+                elif private_key.endswith('dsa') or private_key.count('dsa'):
                     pkey = self._load_dsa_key(private_key, private_key_pass)
                 else:
                     log.warn("specified key does not end in either rsa or dsa, trying both")
@@ -72,6 +82,7 @@ class Connection(object):
         private_key_file = os.path.expanduser(private_key)
         try:
             rsa_key = paramiko.RSAKey.from_private_key_file(private_key_file, private_key_pass)
+            log.info("Using private key %s (rsa)" % private_key)
             return rsa_key
         except paramiko.SSHException,e:
             log.error('invalid rsa key or password specified')
@@ -80,6 +91,7 @@ class Connection(object):
         private_key_file = os.path.expanduser(private_key)
         try:
             dsa_key = paramiko.DSSKey.from_private_key_file(private_key_file, private_key_pass)
+            log.info("Using private key %s (dsa)" % private_key)
             return dsa_key
         except paramiko.SSHException,e:
             log.error('invalid dsa key or password specified')
@@ -138,6 +150,84 @@ class Connection(object):
         if self._tranport_live:
             self._transport.close()
             self._tranport_live = False
+
+
+    def interactive_shell(self):
+        try:
+            chan = self._transport.open_session()
+            chan.get_pty()
+            chan.invoke_shell()
+            log.info('Starting interactive shell...')
+            if HAS_TERMIOS:
+                self._posix_shell(chan)
+            else:
+                self._windows_shell(chan)
+            chan.close()
+        except Exception, e:
+            import traceback
+            print '*** Caught exception: %s: %s' % (e.__class__, e)
+            traceback.print_exc()
+
+    def _posix_shell(self, chan):
+        import select
+        
+        oldtty = termios.tcgetattr(sys.stdin)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            tty.setcbreak(sys.stdin.fileno())
+            chan.settimeout(0.0)
+
+            chan.send('eval $(resize)\n') # needs to be sent to give vim correct size FIX
+
+            while True:
+                r, w, e = select.select([chan, sys.stdin], [], [])
+                if chan in r:
+                    try:
+                        x = chan.recv(1024)
+                        if len(x) == 0:
+                            print '\r\n*** EOF\r\n',
+                            break
+                        sys.stdout.write(x)
+                        sys.stdout.flush()
+                    except socket.timeout:
+                        pass
+                if sys.stdin in r:
+                    x = os.read(sys.stdin.fileno(),1) # fixes up arrow problem
+                    if len(x) == 0:
+                        break
+                    chan.send(x)
+        finally:
+            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, oldtty)
+
+        
+    # thanks to Mike Looijmans for this code
+    def _windows_shell(self, chan):
+        import threading
+
+        sys.stdout.write("Line-buffered terminal emulation. Press F6 or ^Z to send EOF.\r\n\r\n")
+            
+        def writeall(sock):
+            while True:
+                data = sock.recv(256)
+                if not data:
+                    sys.stdout.write('\r\n*** EOF ***\r\n\r\n')
+                    sys.stdout.flush()
+                    break
+                sys.stdout.write(data)
+                sys.stdout.flush()
+            
+        writer = threading.Thread(target=writeall, args=(chan,))
+        writer.start()
+            
+        try:
+            while True:
+                d = sys.stdin.read(1)
+                if not d:
+                    break
+                chan.send(d)
+        except EOFError:
+            # user hit ^Z or F6
+            pass
 
     def __del__(self):
         """Attempt to clean up if not explicitly closed."""
