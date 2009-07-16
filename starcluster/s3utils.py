3f8e369607547f9a013ceebfcf25f72b2d3d34b7
Justin Riley <justin.t.riley@gmail.com>
2009-07-16 17:28:58 -0400

    added logging and connection caching to s3utils
    also added bucket_exists method and incorporated this check for every function in s3utils that has bucket_name as arg

--- starcluster/s3utils.py
+++ starcluster/s3utils.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python
-from starcluster.starclustercfg import *
+import logging
+import starcluster.starclustercfg as cfg
 from starcluster import S3
 
+log = logging.getLogger('starcluster')
+
+S3_CONNECTION = None
+
 def get_conn():
-    return S3.AWSAuthConnection(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+    if S3_CONNECTION is None:
+        log.debug('S3_CONNECTION is None, creating...')
+        globals()['S3_CONNECTION'] = S3.AWSAuthConnection(cfg.AWS_ACCESS_KEY_ID, cfg.AWS_SECRET_ACCESS_KEY)
+    return S3_CONNECTION
+
+def bucket_exists(bucket_name):
+    exists = (get_conn().check_bucket_exists(bucket_name).reason == 'OK')
+    log.error('bucket %s does not exist' % bucket_name)
+    return exists
 
 def get_buckets():
     conn = get_conn()
@@ -13,20 +26,23 @@ def get_buckets():
         buckets.append(bucket.name)
     return buckets
 
-
 def list_buckets():
     for bucket in get_buckets():
         print bucket
 
 def get_bucket_files(bucket_name):
     conn = get_conn()
-    files = [ entry.key for entry in conn.list_bucket(bucket_name).entries] 
+    if bucket_exists(bucket_name):
+        files = [ entry.key for entry in conn.list_bucket(bucket_name).entries] 
+    else:
+        files = []
     return files
 
 def show_bucket_files(bucket_name):
-    files = get_bucket_files(bucket_name)
-    for file in files:
-        print file
+    if bucket_exists(bucket_name):
+        files = get_bucket_files(bucket_name)
+        for file in files:
+            print file
 
 def remove_file(bucket_name, file_name):
     conn = get_conn()
