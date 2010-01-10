304ef05683f55dd5784133cfa6d6efd41f06910c
Justin Riley <justin.t.riley@gmail.com>
2010-01-10 01:46:58 -0500

    code cleanup

--- starcluster/__init__.py
+++ starcluster/__init__.py
@@ -1,9 +1,2 @@
-# StarCluster internal modules
 __version__ = "0.9999"
 __author__ = "Justin Riley (justin.t.riley@gmail.com)"
-#import logger
-#import config
-#import awsutils
-#import ssh
-#import cluster
-#import clustersetup
