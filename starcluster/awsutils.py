697ea278f94d87967d1bc0e523bed7f0552a7ed4
Justin Riley <justin.t.riley@gmail.com>
2010-03-24 16:32:38 -0400

    minor bug fix for eucalyptus s3 support

--- starcluster/awsutils.py
+++ starcluster/awsutils.py
@@ -337,7 +337,7 @@ class EasyS3(EasyAWS):
     _calling_format=boto.s3.connection.OrdinaryCallingFormat()
     def __init__(self, aws_access_key_id, aws_secret_access_key,  
                  aws_s3_path='/', aws_port=None, aws_is_secure=True, 
-                 aws_region_host=DefaultHost, cache=False):
+                 aws_region_host=DefaultHost, cache=False, **kwargs):
         kwargs = dict(is_secure=aws_is_secure, host=aws_region_host, 
                       calling_format=self._calling_format, port=aws_port, 
                       path=aws_s3_path)
