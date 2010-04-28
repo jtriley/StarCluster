827a8613679bb5a97295909ca52f7bf642715860
Justin Riley <justin.t.riley@gmail.com>
2010-04-28 01:18:50 -0400

    add __all__ to starcluster module
    add src to .gitignore. src is usually a symlink to
    starcluster/starcluster folder

--- starcluster/__init__.py
+++ starcluster/__init__.py
@@ -1,5 +1,22 @@
 __version__ = "0.9999"
 __author__ = "Justin Riley (justin.t.riley@gmail.com)"
+__all__ = [
+    "config", 
+    "cli",
+    "awsutils", 
+    "ssh", 
+    "utils", 
+    "static",
+    "exception",
+    "cluster", 
+    "node",
+    "clustersetup", 
+    "image", 
+    "volume", 
+    "tests",
+    "templates",
+    "optcomplete",
+]
 import sys
-import static 
+from starcluster import static 
 sys.path.insert(0,static.STARCLUSTER_PLUGIN_DIR)
