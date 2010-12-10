b00bae3b05ee427dc9789a733b8ea2ee4bb62afc
Justin Riley <justin.t.riley@gmail.com>
2010-12-10 11:28:41 -0500

    fix bug in Cluster.is_cluster_terminated

--- starcluster/cluster.py
+++ starcluster/cluster.py
@@ -1004,7 +1004,8 @@ class Cluster(object):
         Check whether all nodes are in a 'terminated' state
         """
         states = filter(lambda x: x != 'terminated', static.INSTANCE_STATES)
-        filters = {'instance-state-name': states}
+        filters = {'group-id': self._cluster_group,
+                   'instance-state-name': states}
         insts = self.ec2.get_all_instances(filters=filters)
         return len(insts) == 0
 
