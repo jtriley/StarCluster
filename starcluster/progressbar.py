10438ff28961fce79891a9dd30a58723d494d208
Justin Riley <justin.t.riley@gmail.com>
2010-12-21 14:54:21 -0500

    make progressbar examples easier to interact with
    moved examples into the module namespace, modified them to return the
    example progressbars
    
    moved main code into function called main()

--- starcluster/progressbar.py
+++ starcluster/progressbar.py
@@ -317,60 +317,66 @@ class ProgressBar(object):
         if self.signal_set:
             signal.signal(signal.SIGWINCH, signal.SIG_DFL)
 
-if __name__ == '__main__':
+def example1():
+    widgets = ['Test: ', Percentage(), ' ', Bar(marker=RotatingMarker()),
+               ' ', ETA(), ' ', FileTransferSpeed()]
+    pbar = ProgressBar(widgets=widgets, maxval=10000000).start()
+    for i in range(1000000):
+        # do something
+        pbar.update(10 * i + 1)
+    pbar.finish()
+    return pbar
 
-    def example1():
-        widgets = ['Test: ', Percentage(), ' ', Bar(marker=RotatingMarker()),
-                   ' ', ETA(), ' ', FileTransferSpeed()]
-        pbar = ProgressBar(widgets=widgets, maxval=10000000).start()
-        for i in range(1000000):
-            # do something
-            pbar.update(10 * i + 1)
-        pbar.finish()
-        print
+def example2():
+    class CrazyFileTransferSpeed(FileTransferSpeed):
+        "It's bigger between 45 and 80 percent"
+        def update(self, pbar):
+            if 45 < pbar.percentage() < 80:
+                return 'Bigger Now ' + FileTransferSpeed.update(self, pbar)
+            else:
+                return FileTransferSpeed.update(self, pbar)
 
-    def example2():
-        class CrazyFileTransferSpeed(FileTransferSpeed):
-            "It's bigger between 45 and 80 percent"
-            def update(self, pbar):
-                if 45 < pbar.percentage() < 80:
-                    return 'Bigger Now ' + FileTransferSpeed.update(self, pbar)
-                else:
-                    return FileTransferSpeed.update(self, pbar)
+    widgets = [CrazyFileTransferSpeed(), ' <<<',
+               Bar(), '>>> ', Percentage(), ' ', ETA()]
+    pbar = ProgressBar(widgets=widgets, maxval=10000000)
+    # maybe do something
+    pbar.start()
+    for i in range(2000000):
+        # do something
+        pbar.update(5 * i + 1)
+    pbar.finish()
+    return pbar
 
-        widgets = [CrazyFileTransferSpeed(), ' <<<',
-                   Bar(), '>>> ', Percentage(), ' ', ETA()]
-        pbar = ProgressBar(widgets=widgets, maxval=10000000)
-        # maybe do something
-        pbar.start()
-        for i in range(2000000):
-            # do something
-            pbar.update(5 * i + 1)
-        pbar.finish()
-        print
+def example3():
+    widgets = [Bar('>'), ' ', ETA(), ' ', ReverseBar('<')]
+    pbar = ProgressBar(widgets=widgets, maxval=10000000).start()
+    for i in range(1000000):
+        # do something
+        pbar.update(10 * i + 1)
+    pbar.finish()
+    return pbar
 
-    def example3():
-        widgets = [Bar('>'), ' ', ETA(), ' ', ReverseBar('<')]
-        pbar = ProgressBar(widgets=widgets, maxval=10000000).start()
-        for i in range(1000000):
-            # do something
-            pbar.update(10 * i + 1)
-        pbar.finish()
-        print
+def example4():
+    widgets = ['Test: ', Percentage(), ' ',
+               Bar(marker='0', left='[', right=']'),
+               ' ', ETA(), ' ', FileTransferSpeed()]
+    pbar = ProgressBar(widgets=widgets, maxval=500)
+    pbar.start()
+    for i in range(100, 500 + 1, 50):
+        time.sleep(0.2)
+        pbar.update(i)
+    pbar.finish()
+    return pbar
 
-    def example4():
-        widgets = ['Test: ', Percentage(), ' ',
-                   Bar(marker='0', left='[', right=']'),
-                   ' ', ETA(), ' ', FileTransferSpeed()]
-        pbar = ProgressBar(widgets=widgets, maxval=500)
-        pbar.start()
-        for i in range(100, 500 + 1, 50):
-            time.sleep(0.2)
-            pbar.update(i)
-        pbar.finish()
-        print
-
+def main():
     example1()
+    print
     example2()
+    print
     example3()
+    print
     example4()
+    print
+
+if __name__ == '__main__':
+    main()
