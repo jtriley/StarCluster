73f8753d6be07dfe3e038a0dde1b0dc427a2dd2f
Justin Riley <justin.t.riley@gmail.com>
2011-05-17 18:36:33 -0400

    replace clean.sh/check.sh with python versions

---
+++ check.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+import os
+import sys
+import glob
+import pep8
+from pyflakes.scripts import pyflakes
+
+def findpy(path):
+    for cfile in glob.glob(os.path.join(path, '*')):
+        if os.path.isdir(cfile):
+            findpy(cfile)
+        if cfile.endswith('.py'):
+            yield cfile
+
+def check_pyflakes(srcdir):
+    print(">>> Running pyflakes...")
+    clean = True
+    for pyfile in findpy(srcdir):
+        if pyflakes.checkPath(pyfile) != 0:
+            clean = False
+    return clean
+
+def check_pep8(srcdir):
+    print(">>> Running pep8...")
+    clean = True
+    pep8.process_options([''])
+    for pyfile in findpy(srcdir):
+        if pep8.Checker(pyfile).check_all() != 0:
+            clean = False
+    return clean
+
+def main():
+    sc_src=os.path.join(os.path.dirname(sys.argv[0]), 'starcluster')
+    if not check_pyflakes(sc_src):
+        print
+        err = "ERROR: pyflakes failed on some source files\n"
+        err += "ERROR: please fix the errors and re-run this script"
+        print(err)
+    elif not check_pep8(sc_src):
+        print
+        err = "ERROR: pep8 failed on some source files\n"
+        err += "ERROR: please fix the errors and re-run this script"
+        print(err)
+    else:
+        print(">>> Clean!")
+
+if __name__ == '__main__':
+    main()
--- clean.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-SRC=$(dirname $0)/starcluster
-find $SRC -iname \*.pyc -exec rm {} \;
-find $SRC -iname \*.pyo -exec rm {} \;
