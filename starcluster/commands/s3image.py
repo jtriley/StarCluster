e8609f2090dabac79c03216e3df83abb18a10912
Justin Riley <justin.t.riley@gmail.com>
2011-08-02 15:03:34 -0400

    add deprecation warning about createimage command

--- starcluster/commands/s3image.py
+++ starcluster/commands/s3image.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
-
+import sys
 import time
+import warnings
 
 from starcluster import exception
 from starcluster.logger import log
@@ -51,6 +52,10 @@ class CmdS3Image(InstanceCompleter):
         raise exception.CancelledS3ImageCreation(self.bucket, self.image_name)
 
     def execute(self, args):
+        if "createimage" in sys.argv:
+            warnings.warn("createimage is deprecated and will go away in the "
+                          "next release. please use the s3image/ebsimage "
+                          "commands instead", DeprecationWarning)
         if len(args) != 3:
             self.parser.error(
                 'you must specify an instance-id, image name, and bucket')
