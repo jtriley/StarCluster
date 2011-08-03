a3f152974fb13463999eb1632989b88504b8ef67
Justin Riley <justin.t.riley@gmail.com>
2011-08-03 14:11:05 -0400

    addnode: support for adding multiple nodes

--- starcluster/commands/addnode.py
+++ starcluster/commands/addnode.py
@@ -23,20 +23,46 @@ class CmdAddNode(ClusterCompleter):
 
     def addopts(self, parser):
         parser.add_option("-a", "--alias", dest="alias",
-                          action="store", type="string", default=None,
+                          action="append", type="string", default=[],
                           help=("alias to give to the new node " + \
                                 "(e.g. node007, mynode, etc)"))
+        parser.add_option("-n", "--num-nodes", dest="num_nodes",
+                          action="store", type="int", default=1,
+                          help=("number of new nodes to launch"))
         parser.add_option("-x", "--no-create", dest="no_create",
                           action="store_true", default=False,
                           help="do not launch new EC2 instances when " + \
                           "adding nodes (use existing instances instead)")
 
+    def _get_duplicate(self, lst):
+        d = {}
+        for item in lst:
+            if item in d:
+                return item
+            else:
+                d[item] = 0
+
     def execute(self, args):
         if len(args) != 1:
             self.parser.error("please specify a cluster <cluster_tag>")
         tag = self.tag = args[0]
+        aliases = []
+        for alias in self.opts.alias:
+            aliases.extend(alias.split(','))
+        if 'master' in aliases:
+            self.parser.error("'master' is a reserved alias")
+        num_nodes = self.opts.num_nodes
+        if num_nodes == 1 and aliases:
+            num_nodes = len(aliases)
+        if num_nodes > 1 and aliases and len(aliases) != num_nodes:
+            self.parser.error("you must specify the same number of aliases "
+                              "(-a) as nodes (-n)")
+        dupe = self._get_duplicate(aliases)
+        if dupe:
+            self.parser.error("cannot have duplicate aliases (duplicate: %s)" %
+                              dupe)
         if not self.opts.alias and self.opts.no_create:
-            self.parser.error("you must specify a node alias via the -a "
-                              "option when using -x")
-        self.cm.add_node(tag, alias=self.opts.alias,
-                         no_create=self.opts.no_create)
+            self.parser.error("you must specify one or more node aliases via "
+                              "the -a option when using -x")
+        self.cm.add_nodes(tag, num_nodes, aliases=aliases,
+                          no_create=self.opts.no_create)
