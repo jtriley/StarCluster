cb9eccd5f8248e60d6a4fa36844ad35d282c27de
Justin Riley <justin.t.riley@gmail.com>
2011-08-24 14:18:01 -0400

    user_msgs: fix indentation for example commands

--- starcluster/templates/user_msgs.py
+++ starcluster/templates/user_msgs.py
@@ -4,14 +4,14 @@ active_ebs_cluster = """EBS Cluster '%(cluster_name)s' already exists.
 
 Either choose a different tag name, or stop the EBS cluster using:
 
-  $ starcluster stop %(cluster_name)s
+    $ starcluster stop %(cluster_name)s
 
 This command will put all nodes into a 'stopped' state and preserve their \
 local disks. The cluster can later be resumed by passing the -x option to \
 the start command. Another option is to terminate the existing EBS Cluster \
 using:
 
-  $ starcluster terminate %(cluster_name)s
+    $ starcluster terminate %(cluster_name)s
 
 NOTE: Terminating an EBS cluster will destroy the local disks (volumes) \
 backing the nodes.
@@ -21,11 +21,11 @@ stopped_ebs_cluster = """Stopped EBS Cluster '%(cluster_name)s' already exists.
 
 Either choose a different tag name, or start the stopped EBS cluster using:
 
-  $ starcluster start -x %(cluster_name)s
+    $ starcluster start -x %(cluster_name)s
 
 Another option is to terminate the existing EBS Cluster using:
 
-  $ starcluster terminate %(cluster_name)s
+    $ starcluster terminate %(cluster_name)s
 
 NOTE: Terminating an EBS cluster will destroy the local disks (volumes) \
 backing the nodes.
@@ -95,7 +95,7 @@ If this takes too long, you can cancel the start command using CTRL-C. \
 You can then resume the start command later on using the --no-create (-x) \
 option:
 
-   $ starcluster start -x %(tag)s
+    $ starcluster start -x %(tag)s
 
 This will use the existing spot instances launched previously and continue \
 starting the cluster. If you don't wish to wait on the cluster any longer \
