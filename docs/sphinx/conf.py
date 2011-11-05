1c279af608d8b7f9cf2ac6e6de520526a51445df
Justin Riley <justin.t.riley@gmail.com>
2011-11-05 17:26:21 -0400

    docs: update year to 2011 in sphinx conf

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -38,7 +38,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'StarCluster'
-copyright = u'2010, Software Tools for Academics and Researchers'
+copyright = u'2011, Software Tools for Academics and Researchers'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
