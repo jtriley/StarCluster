85e987cdbd1357b8f93653eca1460553369622bf
Justin Riley <justin.t.riley@gmail.com>
2012-02-09 16:01:07 -0500

    Merge pull request #76 from yamins81/feature/addnode_options
    Squashed commit of the following:
    
    commit 9a3fe527c643836b667a5fcf9dbae5c5065fcb21
    Author: Dan Yamins <dyamins@gmail.com>
    Date:   Tue Feb 7 12:20:35 2012 -0500
    
        bugfix about count argument
    
    commit 08174946e3dd6bfa42d13a0585cc44f95050e9cd
    Author: Dan Yamins <dyamins@gmail.com>
    Date:   Mon Feb 6 18:44:22 2012 -0500
    
        commit of addnode options

--- starcluster/commands/addnode.py
+++ starcluster/commands/addnode.py
@@ -59,10 +59,25 @@ class CmdAddNode(ClusterCompleter):
         parser.add_option("-n", "--num-nodes", dest="num_nodes",
                           action="store", type="int", default=1,
                           help="number of new nodes to launch")
+        parser.add_option("-i", "--image-id", dest="image_id",
+                          action="store", type="string", default=None,
+                          help=("image id for new node(s) " + \
+                                "(e.g. ami-12345678)."))
+        parser.add_option("-I", "--instance-type", dest="instance_type",
+                          action="store", type="string", default=None,
+                          help=("instance type for new node(s) " + \
+                                "(e.g. m1.large)"))
+        parser.add_option("-z", "--availability-zone", dest="zone",
+                          action="store", type="string", default=None,
+                          help=("availability zone for new node(s) " + \
+                                "(e.g. us-east-1)"))
+        parser.add_option("-b", "--bid", dest="spot_bid",
+                          action="store", type="float", default=None,
+                          help=("spot bid for new node(s), in $ per hour"))
         parser.add_option("-x", "--no-create", dest="no_create",
                           action="store_true", default=False,
-                          help="do not launch new EC2 instances when "
-                          "adding nodes (use existing instances instead)")
+                          help=("do not launch new EC2 instances when" + \
+                          "adding nodes (use existing instances instead)"))
 
     def _get_duplicate(self, lst):
         d = {}
@@ -76,6 +91,7 @@ class CmdAddNode(ClusterCompleter):
         if len(args) != 1:
             self.parser.error("please specify a cluster <cluster_tag>")
         tag = self.tag = args[0]
+
         aliases = []
         for alias in self.opts.alias:
             aliases.extend(alias.split(','))
@@ -95,4 +111,8 @@ class CmdAddNode(ClusterCompleter):
             self.parser.error("you must specify one or more node aliases via "
                               "the -a option when using -x")
         self.cm.add_nodes(tag, num_nodes, aliases=aliases,
+                          image_id=self.opts.image_id,
+                          instance_type=self.opts.instance_type,
+                          zone=self.opts.zone,
+                          spot_bid=self.opts.spot_bid,
                           no_create=self.opts.no_create)
