8f385296fda8d847493909a9e745a975362a4462
Justin Riley <justin.t.riley@gmail.com>
2012-02-22 13:19:19 -0500

    bump boto dependency to latest 2.2.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ try:
     console_scripts = ['starcluster = starcluster.cli:main']
     extra = dict(test_suite="starcluster.tests",
                  tests_require="nose",
-                 install_requires=["paramiko==1.7.7.1", "boto==2.2.1",
+                 install_requires=["paramiko==1.7.7.1", "boto==2.2.2",
                                    "workerpool==0.9.2", "Jinja2==2.6",
                                    "decorator==3.3.1", "pyasn1==0.0.13b"],
                  include_package_data=True,
