a66fa3cd784c67c8a6027c7fd2fc8d7db71680b2
locojaydev <locojaydev@dev.com>
2012-04-25 18:27:06 -0400

    removing @ from security group prefix, adding vpc_id , subnet_id to cluster settings

--- starcluster/static.py
+++ starcluster/static.py
@@ -85,7 +85,7 @@ BASE_AMI_32 = "ami-7c5c3915"
 BASE_AMI_64 = "ami-765b3e1f"
 BASE_AMI_HVM = "ami-52a0c53b"
 
-SECURITY_GROUP_PREFIX = "@sc"
+SECURITY_GROUP_PREFIX = "sc"
 SECURITY_GROUP_TEMPLATE = '-'.join([SECURITY_GROUP_PREFIX, "%s"])
 VOLUME_GROUP_NAME = "volumecreator"
 VOLUME_GROUP = SECURITY_GROUP_TEMPLATE % VOLUME_GROUP_NAME
@@ -227,6 +227,8 @@ CLUSTER_SETTINGS = {
     'cluster_size': (int, True, None, None, None),
     'cluster_user': (str, False, 'sgeadmin', None, None),
     'cluster_shell': (str, False, 'bash', AVAILABLE_SHELLS.keys(), None),
+    'vpc_id': (str, False, None, None, None),
+    'subnet_id': (str, False, None, None, None),
     'master_image_id': (str, False, None, None, None),
     'master_instance_type': (str, False, None, INSTANCE_TYPES.keys(), None),
     'node_image_id': (str, True, None, None, None),
