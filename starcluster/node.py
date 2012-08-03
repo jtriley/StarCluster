a24ef67aec2b6af89552dd023ebcc2a415b95892
Justin Riley <justin.t.riley@gmail.com>
2012-08-03 16:00:31 -0400

    node: make nm.ssh_to_node a proxy to node.shell()
    NodeManager.ssh_to_node had duplicate code already provided by
    Node.shell and also did not support the new X11 or Agent forwarding
    options added to sshmaster/sshnode commands. Updated this method to
    simply fetch the node and proxy all args to node.shell(). Added missing
    forward_x11 and forward_agent kwargs to NodeManager.ssh_to_node.

--- starcluster/node.py
+++ starcluster/node.py
@@ -18,16 +18,11 @@ class NodeManager(managers.Manager):
     """
     Manager class for Node objects
     """
-    def ssh_to_node(self, node_id, user='root', command=None):
+    def ssh_to_node(self, node_id, user='root', command=None,
+                    forward_x11=False, forward_agent=False):
         node = self.get_node(node_id, user=user)
-        if command:
-            current_user = node.ssh.get_current_user()
-            node.ssh.switch_user(user)
-            node.ssh.execute(command, silent=False)
-            node.ssh.switch_user(current_user)
-            return node.ssh.get_last_status()
-        else:
-            node.shell(user=user)
+        return node.shell(user=user, command=command, forward_x11=forward_x11,
+                          forward_agent=forward_agent)
 
     def get_node(self, node_id, user='root'):
         """Factory for Node class"""
