c70bebc07e7ba5dd96994d1319c0100fe3c41e2d
Justin Riley <justin.t.riley@gmail.com>
2012-09-26 12:12:27 -0400

    tests: update cloudinit tests for latest changes

--- starcluster/tests/test_cloudinit_utils.py
+++ starcluster/tests/test_cloudinit_utils.py
@@ -1,17 +1,35 @@
+from starcluster import utils
 from starcluster import cloudinit
 
 
-def _test_mp_userdata_utils(compress=False):
-    scstore, bscript = '#sc-store\nhi', '#!/bin/bash\nthere'
-    userdata = cloudinit.mp_userdata_from_strings([scstore, bscript],
-                                                  compress=compress)
-    mp = cloudinit.get_mp_from_userdata(userdata, decompress=compress)
-    store, script, phandler = mp.get_payload()
-    assert store.get_payload() == scstore
-    assert script.get_payload() == bscript
-    assert phandler.get_payload() == cloudinit.sc_part_handler
+def _test_bundle_userdata(compress=False, use_cloudinit=True):
+    ignored = '#ignored\nThis file will not be executed.'
+    bscript = '#!/bin/bash\hostname'
+    files = utils.strings_to_files([ignored, bscript], fname_prefix='sc')
+    userdata = cloudinit.bundle_userdata_files(files, compress=compress,
+                                               use_cloudinit=use_cloudinit)
+    unbundled = cloudinit.unbundle_userdata(userdata, decompress=compress)
+    if use_cloudinit:
+        cloud_cfg = unbundled.get('starcluster_cloud_config.txt')
+        assert cloud_cfg.startswith('#cloud-config')
+    else:
+        enable_root = unbundled.get('starcluster_enable_root_login.sh')
+        assert enable_root == cloudinit.ENABLE_ROOT_LOGIN_SCRIPT
+    # ignored files should have #!/bin/false prepended automagically
+    ilines = unbundled.get('sc_0').splitlines()
+    assert ilines[0] == '#!/bin/false'
+    # removing the auto-inserted #!/bin/false should get us back to the
+    # original ignored script
+    ignored_mod = '\n'.join(ilines[1:])
+    assert ignored == ignored_mod
+    # check that second file is bscript
+    assert unbundled.get('sc_1') == bscript
 
 
-def test_mp_userdata_utils():
-    _test_mp_userdata_utils(compress=False)
-    _test_mp_userdata_utils(compress=True)
+def test_cloudinit():
+    _test_bundle_userdata(compress=False)
+    _test_bundle_userdata(compress=True)
+
+
+def test_non_cloudinit():
+    _test_bundle_userdata(use_cloudinit=False)
