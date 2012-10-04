d135f333e2817b83a9cf2f55d2f85b8b94b8b7ad
Justin Riley <justin.t.riley@gmail.com>
2012-10-04 13:46:24 -0400

    scimage: fix typo in default package list

--- utils/scimage.py
+++ utils/scimage.py
@@ -509,7 +509,7 @@ mysql-server mysql-server/root_password_again seen true
     pkgs += "python-virtualenv python-imaging python-boto python-matplotlib "
     pkgs += "unzip rar unace build-essential gfortran ec2-api-tools "
     pkgs += "ec2-ami-tools mysql-server mysql-client apache2 "
-    pkgs += "libapache2-mod-wsgi sysv-rc-conf pssh emacs cython irssi htop"
+    pkgs += "libapache2-mod-wsgi sysv-rc-conf pssh emacs cython irssi htop "
     pkgs += "python-distutils-extra vim-scripts python-ctypes python-pudb "
     pkgs += "mosh python-scipy python-numpy default-jdk"
     apt_install(pkgs)
