00545b38caf3456762c3d8b64def0ba33c34d5df
Mich <fmlheureux@recoset.com>
2012-12-04 11:32:20 -0500

    BLUK-27, using complex values should prevent node overload, no more slots=1 on nodes.

--- starcluster/plugins/datacratic_post.py
+++ starcluster/plugins/datacratic_post.py
@@ -7,16 +7,22 @@ from starcluster.logger import log
 
 class DatacraticPostPlugin(clustersetup.DefaultClusterSetup):
 
+    _dcePath = "/usr/bin/datacraticCopyEditor"
+
     def __init__(self, **kwargs):
         pass
     
     def run(self, nodes, master, user, user_shell, volumes):
-        log.info("Datacratic plugin: setting master to 0 slot")
+        log.info("Setting master to 0 slot")
         self._set_node_slots(master, "master", 0)
+        self._create_dce(master, True)
+        log.info("Creating complex values configuration")
+        self._create_complex_values(master)
 
     def on_add_node(self, node, nodes, master, user, user_shell, volumes):
-        log.info("Overriding SGE node config to set slots=1")
-        self._set_node_slots(master, node.alias, 1)
+        self._create_dce(master)
+        log.info("Configuring complex values for " + node.alias)
+        self._update_complex_values(master, node)
 
     def on_remove_node(self, node, nodes, master, user, user_shell, volumes):
         pass
@@ -24,20 +30,20 @@ class DatacraticPostPlugin(clustersetup.DefaultClusterSetup):
     def clean_cluster(self, nodes, master, user, user_shell, volumes):
         pass
 
-
-    def _set_node_slots(self, master, node_alias, num_slots):
-        dcePath = "/usr/bin/datacraticCopyEditor"
-        qconfPath = "/root/queueconfig.qconf"
-        if not master.ssh.path_exists(dcePath):
+    def _create_dce(self, master, force=False):
+        if not master.ssh.path_exists(self._dcePath) or force:
             #TODO: nice hack, complete it by doing the entire stuff remotely
-            dce = master.ssh.remote_file(dcePath, "w")
-            dce.write("#!/bin/bash\ncp $1 " + qconfPath + "\n")
+            dce = master.ssh.remote_file(self._dcePath, "w")
+            dce.write("#!/bin/bash\ncp $1 $DCE_DEST\n")
             dce.close()
+    
+    def _set_node_slots(self, master, node_alias, num_slots):
+        qconfPath = "/root/queueconfig.qconf"
         #with our copy editor, the current config is printed to a file
-        master.ssh.execute("export EDITOR=" + dcePath + "; "\
+        master.ssh.execute("export EDITOR=" + self._dcePath + "; "\
             + "chmod +x $EDITOR; "\
-            + "echo $EDITOR; "\
-            + "qconf -mq all.q", source_profile=True)
+            + "export DCE_DEST=" + qconfPath + "; "\
+            + "qconf -mq all.q")
         qconf = master.ssh.remote_file(qconfPath, "r")
         qconfStr = qconf.read()
         qconf.close()
@@ -51,6 +57,46 @@ class DatacraticPostPlugin(clustersetup.DefaultClusterSetup):
             qconf = master.ssh.remote_file(qconfPath, "w")
             qconf.write(qconfStr)
             qconf.close()
-            master.ssh.execute("qconf -Mq " + qconfPath, 
-                source_profile=True)
+            master.ssh.execute("qconf -Mq " + qconfPath)
 
+    def _create_complex_values(self, master):
+        """
+        Defines complex values in OGS
+        """
+        dest = "/root/qconf_mc.qconf"
+        master.ssh.execute("export EDITOR=" + self._dcePath + "; "\
+            + "export DCE_DEST=" + dest + "; "\
+            + "qconf -mc")
+        qconf = master.ssh.remote_file(dest, "r")
+        qconfStr = qconf.read()
+        qconf.close()
+        qconfStr += "da_exclusive da_excl INT <= YES YES 0 0\n"\
+            + "da_mem_gb da_mem_gb DOUBLE <= YES YES 0 0\n"\
+            + "da_slots da_slots INT <= YES YES 0 0\n"
+        qconf = master.ssh.remote_file(dest, "w")
+        qconf.write(qconfStr)
+        qconf.close()
+        master.ssh.execute("qconf -Mc " + dest)
+
+    def _update_complex_values(self, master, node):
+        """
+        Sets complex values values for a node
+        """
+        dest = "/root/qconf_me_" + node.alias + ".qconf"
+        master.ssh.execute("export EDITOR=" + self._dcePath +"; "\
+            + "export DCE_DEST=" + dest + "; "\
+            + "qconf -me " + node.alias, ignore_exit_status=True)
+        qconf = master.ssh.remote_file(dest, "r")
+        qconfStr = qconf.read()
+        qconf.close()
+        qconfLines = qconfStr.split("\n")
+        for index, line in enumerate(qconfLines):
+            if line.find("complex_values") == 0:
+                qconfLines[index] = "complex_values da_mem_gb="\
+                    + str(float(node.memory)/1000) + ",da_slots="\
+                    + str(node.num_processors) + ",da_exclusive=1"
+                break
+        qconf = master.ssh.remote_file(dest, "w")
+        qconf.write("\n".join(qconfLines))
+        qconf.close()
+        master.ssh.execute("qconf -Me " + dest)
