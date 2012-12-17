2fee7592a0fe0cd8b5951641e2258c3ae3aba5ce
Justin Riley <justin.t.riley@gmail.com>
2012-12-17 12:53:19 -0500

    cluster: simplify instance type/AMI validation code
    Simplified the instance type/AMI validation in __check_platform by
    making use of new static.HVM_TYPES. This refactoring results in the same
    exact validation checks only it's *much* easier to read.

--- starcluster/cluster.py
+++ starcluster/cluster.py
@@ -1724,19 +1724,14 @@ class ClusterValidator(validators.Validator):
                                                    image_id)
         image_platform = image.architecture
         image_is_hvm = (image.virtualization_type == "hvm")
-        instance_is_hvm = instance_type in static.CLUSTER_TYPES
-        instance_is_hi_io = instance_type in static.HI_IO_TYPES
-        instance_is_sec_gen = instance_type in static.SEC_GEN_TYPES
-        if (image_is_hvm and not instance_is_hvm and not 
-            instance_is_hi_io and not instance_is_sec_gen):
-            cctypes_list = ', '.join(static.SEC_GEN_TYPES + static.CLUSTER_TYPES +
-                                     static.HI_IO_TYPES)
+        if image_is_hvm and instance_type not in static.HVM_TYPES:
+            cctypes_list = ', '.join(static.HVM_TYPES)
             raise exception.ClusterValidationError(
                 "Image '%s' is a hardware virtual machine (HVM) image and "
                 "cannot be used with instance type '%s'.\n\nHVM images "
                 "require one of the following HVM instance types:\n%s" %
                 (image_id, instance_type, cctypes_list))
-        if instance_is_hvm and not image_is_hvm:
+        if instance_type in static.CLUSTER_TYPES and not image_is_hvm:
             raise exception.ClusterValidationError(
                 "The '%s' instance type can only be used with hardware "
                 "virtual machine (HVM) images. Image '%s' is not an HVM "
