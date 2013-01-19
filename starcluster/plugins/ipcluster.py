f1793874fa15db0e42c75085980417fffc6065d1
Olivier Grisel <olivier.grisel@ensta.org>
2013-01-19 09:28:09 +0100

    Draft ipcluster plugin refactoring to drop SGE requirement.
    The NFS partition is used instead to be able to run:
    
        ipcluster engines -n numproc
    
    on each non-master node of the cluster.

--- starcluster/plugins/ipcluster.py
+++ starcluster/plugins/ipcluster.py
@@ -1,12 +1,11 @@
 """
-A starcluster plugin for running an IPython cluster using SGE
-(requires IPython 0.11+pyzmq or 0.10+twisted)
-
-See ipythondev plugin for installing git master IPython and its dependencies
+A starcluster plugin for running an IPython cluster
+(requires IPython 0.13+)
 """
 import os
 import time
 import posixpath
+from threading import Thread
 
 from starcluster import utils
 from starcluster import static
@@ -18,58 +17,22 @@ from starcluster.logger import log
 
 IPCLUSTER_CACHE = os.path.join(static.STARCLUSTER_CFG_DIR, 'ipcluster')
 
-STARTED_MSG_10 = """\
-IPCluster has been started on %(cluster)s for user '%(user)s'.
 
-See the IPython 0.10.* parallel docs for usage details
-(http://ipython.org/ipython-doc/rel-0.10.2/html/parallel)
-"""
-
-
-class IPCluster10(ClusterSetup):
-    """
-    Starts an IPCluster (0.10.*) on StarCluster
-    """
-    cluster_file = '/etc/clusterfile.py'
-    log_file = '/var/log/ipcluster.log'
-
-    def _create_cluster_file(self, master, nodes):
-        engines = {}
-        for node in nodes:
-            engines[node.alias] = node.num_processors
-        cfile = 'send_furl = True\n'
-        cfile += 'engines = %s\n' % engines
-        f = master.ssh.remote_file(self.cluster_file, 'w')
-        f.write(cfile)
-        f.close()
-
-    def run(self, nodes, master, user, user_shell, volumes):
-        self._create_cluster_file(master, nodes)
-        log.info("Starting ipcluster...")
-        master.ssh.execute(
-            "su - %s -c 'screen -d -m ipcluster ssh --clusterfile %s'" %
-            (user, self.cluster_file))
-        log.info(STARTED_MSG_10 % dict(cluster=master.parent_cluster,
-                                       user=user))
-
-    def on_add_node(self, node, nodes, master, user, user_shell, volumes):
-        log.info("Adding %s to ipcluster" % node.alias)
-        self._create_cluster_file(master, nodes)
-        user_home = node.getpwnam(user).pw_dir
-        furl_file = posixpath.join(user_home, '.ipython', 'security',
-                                   'ipcontroller-engine.furl')
-        node.ssh.execute(
-            "su - %s -c 'screen -d -m ipengine --furl-file %s'" %
-            (user, furl_file))
-
-    def on_remove_node(self, node, nodes, master, user, user_shell, volumes):
-        log.info("Removing %s from ipcluster" % node.alias)
-        less_nodes = filter(lambda x: x.id != node.id, nodes)
-        self._create_cluster_file(master, less_nodes)
-        node.ssh.execute('pkill ipengine')
+def threaded_call(f, all_args, join=True):
+    """Run a function in parallell"""
+    threads = []
+    for args in all_args:
+        t = Thread(target=f, args=args)
+        t.start()
+        threads.append(t)
+    if join:
+        for t in threads:
+            t.join()
+    else:
+        return threads
 
 
-STARTED_MSG_11 = """\
+STARTED_MSG = """\
 IPCluster has been started on %(cluster)s for user '%(user)s'.
 
 See the IPCluster plugin doc for usage details:
@@ -77,19 +40,22 @@ http://star.mit.edu/cluster/docs/latest/plugins/ipython.html
 """
 
 
-class IPCluster11(ClusterSetup):
-    """
-    Start an IPython (>= 0.11) cluster
-    """
+class IPCluster(ClusterSetup):
+    """Start an IPython (>= 0.11) cluster"""
     def __init__(self, enable_notebook=False, notebook_passwd=None):
         self.enable_notebook = enable_notebook
         self.notebook_passwd = notebook_passwd or utils.generate_passwd(16)
 
+    def _check_ipython_installed(self, node):
+        has_ipy = node.ssh.has_required(['ipython', 'ipcluster'])
+        if not has_ipy:
+            log.error("IPython is not installed... skipping plugin")
+        return has_ipy
+
     def _write_config(self, master, user, profile_dir):
-        """
-        Create cluster config
-        """
+        """Create cluster configuration files."""
         log.info("Writing IPython cluster config files")
+        master.ssh.execute("rm -rf '%s'" % profile_dir)
         master.ssh.execute('ipython profile create')
         f = master.ssh.remote_file('%s/ipcontroller_config.py' % profile_dir)
         ssh_server = "@".join([user, master.public_dns_name])
@@ -98,41 +64,6 @@ class IPCluster11(ClusterSetup):
             "c.HubFactory.ip='%s'" % master.private_ip_address,
             "c.IPControllerApp.ssh_server='%s'" % ssh_server,
             # "c.Application.log_level = 'DEBUG'",
-            "",
-        ]))
-        f.close()
-        f = master.ssh.remote_file('%s/ipcluster_config.py' % profile_dir)
-        f.write('\n'.join([
-            "c = get_config()",
-            "c.IPClusterStart.controller_launcher_class=" +
-            "'SGEControllerLauncher'",
-            # restrict controller to master node:
-            "c.SGEControllerLauncher.queue='all.q@master'",
-            "c.IPClusterEngines.engine_launcher_class='SGEEngineSetLauncher'",
-            # "c.Application.log_level = 'DEBUG'",
-            "",
-            # workaround IPython bug #2171 in IPython 0.13
-            "controller_template = '''",
-            "#$ -V",
-            "#$ -q all.q@master",
-            "#$ -S /bin/sh",
-            "#$ -N ipcontroller",
-            'ipcontroller --log-to-file --profile-dir="{profile_dir}" '
-            '--cluster-id="{cluster_id}"',
-            "'''",
-            "engine_template = '''",
-            "#$ -V",
-            "#$ -t 1-{n}",
-            "#$ -S /bin/sh",
-            "#$ -N ipengine",
-            'ipengine --log-to-file --profile-dir="{profile_dir}" '
-            '--cluster-id="{cluster_id}"',
-            "'''",
-            # only apply workaround for affected version 0.13:
-            "import IPython",
-            "if IPython.__version__ == '0.13':",
-            "    c.SGEControllerLauncher.batch_template = controller_template",
-            "    c.SGEEngineSetLauncher.batch_template = engine_template",
             "",
         ]))
         f.close()
@@ -161,18 +92,21 @@ class IPCluster11(ClusterSetup):
             "    c.Session.unpacker='msgpack.unpackb'",
             "c.EngineFactory.timeout = 10",
             # Engines should wait a while for url files to arrive,
-            # in case Controller takes a bit to start via SGE
+            # in case Controller takes a bit to start
             "c.IPEngineApp.wait_for_url_file = 30",
             # "c.Application.log_level = 'DEBUG'",
             "",
         ]))
         f.close()
 
-    def _start_cluster(self, master, n, profile_dir):
-        log.info("Starting IPython cluster with %i engines" % n)
+    def _start_cluster(self, master, profile_dir):
+        n_engines = max(1, master.num_processors - 1)
+        log.info("Starting IPython cluster with %i engines on master"
+                 % n_engines)
         # cleanup existing connection files, to prevent their use
         master.ssh.execute("rm -f %s/security/*.json" % profile_dir)
-        master.ssh.execute("ipcluster start --n=%i --delay=5 --daemonize" % n)
+        master.ssh.execute("ipcluster start --n=%i --delay=5 --daemonize"
+                           % n_engines)
         # wait for JSON file to exist
         json = '%s/security/ipcontroller-client.json' % profile_dir
         log.info("Waiting for JSON connector file...",
@@ -181,7 +115,7 @@ class IPCluster11(ClusterSetup):
         s.start()
         try:
             while not master.ssh.isfile(json):
-                time.sleep(5)
+                time.sleep(1)
         finally:
             s.stop()
         # retrieve JSON connection info
@@ -244,15 +178,25 @@ class IPCluster11(ClusterSetup):
 
     @print_timing("IPCluster")
     def run(self, nodes, master, user, user_shell, volumes):
-        n = sum([node.num_processors for node in nodes]) - 1
-        user_home = node.getpwnam(user).pw_dir
+        if not self._check_ipython_installed(master):
+            return
+        user_home = master.getpwnam(user).pw_dir
         profile_dir = posixpath.join(user_home, '.ipython', 'profile_default')
         master.ssh.switch_user(user)
         self._write_config(master, user, profile_dir)
-        cfile = self._start_cluster(master, n, profile_dir)
+
+        # Start the cluster and some engines on the master (leave 1
+        # processor free to handle cluster house keeping)
+        cfile = self._start_cluster(master, profile_dir)
+
+        # Start engines on each of the non-master nodes
+        start_engine_args = [(node, user) for node in nodes
+                             if not node.is_master()]
+        threaded_call(self._start_engines, start_engine_args)
+
         if self.enable_notebook:
             self._start_notebook(master, user, profile_dir)
-        log.info(STARTED_MSG_11 % dict(cluster=master.parent_cluster,
+        log.info(STARTED_MSG % dict(cluster=master.parent_cluster,
                                        user=user, connector_file=cfile,
                                        key_location=master.key_location))
         master.ssh.switch_user('root')
@@ -262,53 +206,23 @@ class IPCluster11(ClusterSetup):
         master.ssh.execute("pkill -f ipcontrollerapp.py")
 
     def on_add_node(self, node, nodes, master, user, user_shell, volumes):
-        n = node.num_processors
-        log.info("Adding %i engines on %s to ipcluster" % (n, node.alias))
-        node.ssh.execute("ipcluster engines --n=%i --daemonize" % n)
-
-
-class IPCluster(ClusterSetup):
-
-    def __init__(self, enable_notebook=False, notebook_passwd=None):
-        self.enable_notebook = enable_notebook
-        self.notebook_passwd = notebook_passwd
-
-    def _get_ipy_version(self, node):
-        version_cmd = "python -c 'import IPython; print IPython.__version__'"
-        return node.ssh.execute(version_cmd)[0]
-
-    def _get_ipcluster_plugin(self, node):
-        ipyversion = self._get_ipy_version(node)
-        if ipyversion < '0.11':
-            if not ipyversion.startswith('0.10'):
-                log.warn("Trying unsupported IPython version %s" % ipyversion)
-            return IPCluster10()
-        else:
-            return IPCluster11(self.enable_notebook, self.notebook_passwd)
-
-    def _check_ipython_installed(self, node):
-        has_ipy = node.ssh.has_required(['ipython', 'ipcluster'])
-        if not has_ipy:
-            log.error("IPython is not installed...skipping plugin")
-        return has_ipy
-
-    def run(self, nodes, master, user, user_shell, volumes):
-        if not self._check_ipython_installed(master):
-            return
-        plug = self._get_ipcluster_plugin(master)
-        plug.run(nodes, master, user, user_shell, volumes)
-
-    def on_add_node(self, node, nodes, master, user, user_shell, volumes):
-        if not self._check_ipython_installed(master):
-            return
-        plug = self._get_ipcluster_plugin(master)
-        plug.on_add_node(node, nodes, master, user, user_shell, volumes)
+       if not self._check_ipython_installed(master):
+           return
+       self._start_engines(node, user)
 
     def on_remove_node(self, node, nodes, master, user, user_shell, volumes):
         if not self._check_ipython_installed(master):
             return
-        plug = self._get_ipcluster_plugin(master)
-        plug.on_remove_node(node, nodes, master, user, user_shell, volumes)
+        # TODO: is there a better way?
+        log.info("Removing %s from ipcluster" % node.alias)
+        node.ssh.execute('pkill -f ipengineapp.py')
+
+    def _start_engines(self, node, user):
+        n = node.num_processors
+        log.info("Adding %i engines on %s to ipcluster" % (n, node.alias))
+        node.ssh.switch_user(user)
+        node.ssh.execute("ipcluster engines --n=%i --daemonize" % n)
+        node.ssh.switch_user('root')
 
 
 class IPClusterStop(ClusterSetup):
