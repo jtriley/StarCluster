adf39258760e59935ad74137c45d47a6e2dbb675
Justin Riley <justin.t.riley@gmail.com>
2013-07-08 16:05:40 -0400

    node: improve get_partition_map function
    Updated the code to properly handle the 'boot' flag and hex system id
    output from fdisk. Also improved the partition matching regular
    expression to handle a variety of device formats (e.g. /dev/hda,
    /dev/sda, /dev/sdj1, /dev/cciss/c0d0, etc)

--- starcluster/node.py
+++ starcluster/node.py
@@ -736,18 +736,19 @@ class Node(object):
             devmap[dev] = int(r.findall(proc_parts)[0])
         return devmap
 
-    def get_partition_map(self):
+    def get_partition_map(self, device=None):
         """
         Returns a dictionary mapping partitions->(start, end, blocks, id) based
         on 'fdisk -l'
         """
-        fdiskout = '\n'.join(self.ssh.execute("fdisk -l 2>/dev/null"))
-        part_regex = '/dev/(?:xv|s)d[a-z]\d+(?:p\d+)?'
-        r = re.compile('(%s)\s+'
+        fdiskout = '\n'.join(self.ssh.execute("fdisk -l %s 2>/dev/null" %
+                                              (device or '')))
+        part_regex = '/dev/[A-Za-z0-9/]+'
+        r = re.compile('(%s)\s+\*?\s+'
                        '(\d+)(?:[-+])?\s+'
                        '(\d+)(?:[-+])?\s+'
                        '(\d+)(?:[-+])?\s+'
-                       '(\d+)(?:[-+])?' % part_regex)
+                       '([\da-fA-F][\da-fA-F]?)' % part_regex)
         partmap = {}
         for match in r.findall(fdiskout):
             part, start, end, blocks, sys_id = match
