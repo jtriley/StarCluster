edf1fc568eee9854b5bc93ff2340bec67bbf7f45
Justin Riley <justin.t.riley@gmail.com>
2013-07-08 23:15:57 -0400

    cluster: attach all external EBS volumes at once
    The previous code would attach and wait for each volume individually.
    Updated the code to speed this process up by attaching all volumes at
    once and waiting for them afterwards.

--- starcluster/cluster.py
+++ starcluster/cluster.py
@@ -1312,6 +1312,7 @@ class Cluster(object):
         """
         Attach each volume to the master node
         """
+        wait_for_volumes = []
         for vol in self.volumes:
             volume = self.volumes.get(vol)
             device = volume.get('device')
@@ -1329,6 +1330,8 @@ class Cluster(object):
                      (vol.id, device))
             resp = vol.attach(self.master_node.id, device)
             log.debug("resp = %s" % resp)
+            wait_for_volumes.append(vol)
+        for vol in wait_for_volumes:
             self.ec2.wait_for_volume(vol, state='attached')
 
     def detach_volumes(self):
