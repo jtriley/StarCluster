54690a13f70b25895ccf2ea67bbe7b379463a87b
Justin Riley <justin.t.riley@gmail.com>
2013-12-19 13:35:00 -0500

    removenode: update usage to be similar to addnode
    The removenode command can now be used similar to the addnode command,
    e.g.:
    
    $ starcluster rn mycluster # selects and removes a single node
    $ starcluster rn mycluster -n 3 # selects and removes three nodes
    $ starcluster rn mycluster -a node001,node002 # remove specific nodes
    
    Deprecated the old usage where users passed a list of node aliases to
    remove as arguments, e.g.:
    
    $ starcluster rn mycluster node001 node002 node003
    
    This still works but prompts a warning for now. Support for this
    deprecated usage will be removed in a future release.

--- starcluster/commands/removenode.py
+++ starcluster/commands/removenode.py
@@ -14,6 +14,7 @@
 #
 # You should have received a copy of the GNU Lesser General Public License
 # along with StarCluster. If not, see <http://www.gnu.org/licenses/>.
+import warnings
 
 from starcluster.logger import log
 from starcluster.commands.completers import ClusterCompleter
@@ -21,28 +22,40 @@ from starcluster.commands.completers import ClusterCompleter
 
 class CmdRemoveNode(ClusterCompleter):
     """
-    removenode [options] <cluster_tag> <node> [<node> ...]
+    removenode [options] <cluster_tag>
 
     Terminate one or more nodes in the cluster
 
     Examples:
 
-        $ starcluster removenode mycluster node003
+        $ starcluster removenode mycluster
 
-    This will remove node003 from mycluster and terminate the node.
+    This will automatically fetch a single worker node, detach it from the
+    cluster, and then terminate it. If you'd rather be specific about which
+    node(s) to remove then use the -a option:
+
+        $ starcluster removenode mycluster -a node003
 
     You can also specify multiple nodes to remove and terminate one after
-    another:
+    another, e.g.:
 
-        $ starcluster removenode mycluster node001 node002 node003
+        $ starcluster removenode mycluster -n 3
+
+    or
+
+        $ starcluster removenode mycluster -a node001,node002,node003
 
     If you'd rather not terminate the node(s) after detaching from the cluster,
     use the -k option:
 
-        $ starcluster removenode -k mycluster node001 node002 node003
+        $ starcluster removenode -k mycluster -a node001,node002,node003
 
-    This will remove the nodes from the cluster but leave the instances
-    running. This can be useful, for example, when testing on_add_node and
+    This will detach the nodes from the cluster but leave the instances
+    running. These nodes can then later be reattached to the cluster using:
+
+        $ starcluster addnode mycluster -x -a node001,node002,node003
+
+    This can be useful, for example, when testing on_add_node and
     on_remove_node methods in a StarCluster plugin.
     """
     names = ['removenode', 'rn']
@@ -61,18 +74,54 @@ class CmdRemoveNode(ClusterCompleter):
                           action="store_true", default=False,
                           help="Do not prompt for confirmation, "
                           "just remove the node(s)")
+        parser.add_option("-n", "--num-nodes", dest="num_nodes",
+                          action="store", type="int", default=1,
+                          help="number of nodes to remove")
+        parser.add_option("-a", "--aliases", dest="aliases", action="append",
+                          type="string", default=[],
+                          help="list of nodes to remove (e.g. "
+                          "node001,node002,node003)")
 
     def execute(self, args):
-        if not len(args) >= 2:
-            self.parser.error("please specify a <cluster_tag> and <node>")
+        if not len(args) >= 1:
+            self.parser.error("please specify a cluster <cluster_tag>")
+        if len(args) >= 2:
+            warnings.warn(
+                "Passing node names as arguments is deprecated. Please "
+                "start using the -a option. Pass --help for more details",
+                DeprecationWarning)
         tag = self.tag = args[0]
-        aliases = args[1:]
+        aliases = []
+        for alias in self.opts.aliases:
+            aliases.extend(alias.split(','))
+        if ('master' in aliases) or ('%s-master' % tag in aliases):
+            self.parser.error(
+                "'master' and '%s-master' are reserved aliases" % tag)
+        old_form_aliases = args[1:]
+        if old_form_aliases:
+            if aliases:
+                self.parser.error(
+                    "you must either use a list of nodes as arguments OR "
+                    "use the -a option - not both")
+            else:
+                aliases = old_form_aliases
+        num_nodes = self.opts.num_nodes
+        if num_nodes == 1 and aliases:
+            num_nodes = len(aliases)
+        if num_nodes > 1 and aliases and len(aliases) != num_nodes:
+            self.parser.error("you must specify the same number of aliases "
+                              "(-a) as nodes (-n)")
+        dupe = self._get_duplicate(aliases)
+        if dupe:
+            self.parser.error("cannot have duplicate aliases (duplicate: %s)" %
+                              dupe)
         if not self.opts.confirm:
             resp = raw_input("Remove %s from %s (y/n)? " %
-                             (', '.join(aliases), tag))
+                             (', '.join(aliases) or '%s nodes' % num_nodes,
+                              tag))
             if resp not in ['y', 'Y', 'yes']:
                 log.info("Aborting...")
                 return
-        for alias in aliases:
-            self.cm.remove_node(tag, alias, terminate=self.opts.terminate,
-                                force=self.opts.force)
+        self.cm.remove_nodes(tag, aliases=aliases, num_nodes=num_nodes,
+                             terminate=self.opts.terminate,
+                             force=self.opts.force)
