69e564fed4f7f29a17d820e8c2a6d76bef7cb187
Justin Riley <justin.t.riley@gmail.com>
2014-02-10 18:01:05 -0500

    tests: check pep8 compliance via pytest-pep8

--- starcluster/tests/conftest.py
+++ starcluster/tests/conftest.py
@@ -14,9 +14,6 @@ def pytest_runtest_setup(item):
 
 
 def pytest_configure(config):
-    config.option.exitfirst = True
-    config.option.verbose = True
-    config.option.capture = 'no'
     if config.getoption("--coverage"):
         config.option.cov_source = ['starcluster']
         config.option.cov_report = ['term-missing']
