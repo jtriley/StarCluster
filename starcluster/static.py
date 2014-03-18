f760cff898f0815c9b285f14bb753bf565045767
Justin Riley <justin.t.riley@gmail.com>
2014-03-18 13:02:20 -0400

    bump version to 0.95.3

--- starcluster/static.py
+++ starcluster/static.py
@@ -55,7 +55,7 @@ def create_sc_config_dirs():
     __makedirs(STARCLUSTER_LOG_DIR)
 
 
-VERSION = "0.95.2"
+VERSION = "0.95.3"
 PID = os.getpid()
 TMP_DIR = tempfile.gettempdir()
 if os.path.exists("/tmp"):
