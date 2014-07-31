9788127cba4f8948170810fa5b7c702ff7a79829
Mich <fmlheureux@datacratic.com>
2014-07-31 11:21:50 -0400

    Removed print

--- starcluster/node.py
+++ starcluster/node.py
@@ -210,7 +210,6 @@ class Node(object):
     def get_plugins(self, order, plugins_metadata=None):
         if plugins_metadata is None:
             plugins_metadata = self.get_plugins_full_metadata(order)
-        print plugins_metadata
         plugs = []
         for klass, args, kwargs in plugins_metadata:
             mod_path, klass_name = klass.rsplit('.', 1)
