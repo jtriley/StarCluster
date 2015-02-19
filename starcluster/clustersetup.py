36c59e2c89089c1bfba418ca13059f1a6fe7aeb8
Eduardo G Valente <evalente@navteca.com>
2015-02-19 21:20:51 +0000

    Revert "Update clustersetup.py"
    This reverts commit 05ce930288e17843d5f1c5c47374e5f4c685d982.

--- starcluster/clustersetup.py
+++ starcluster/clustersetup.py
@@ -258,7 +258,7 @@ class DefaultClusterSetup(ClusterSetup):
         """
         log.info("Configuring passwordless ssh for root")
         master = self._master
-        nodes = nodes or self._nodes
+        nodes = nodes or self.nodes
         master.generate_key_for_user('root', auth_new_key=True,
                                      auth_conn_key=True)
         master.enable_passwordless_ssh('root', nodes)
