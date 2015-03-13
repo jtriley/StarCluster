ca6933854ee4742d7fefe422c934516372bbd5e9
Justin Riley <justin.t.riley@gmail.com>
2015-03-13 10:39:16 -0400

    pep8 fix

--- starcluster/cluster.py
+++ starcluster/cluster.py
@@ -2051,7 +2051,8 @@ class ClusterValidator(validators.Validator):
                 if from_port != -1 or to_port != -1:
                     raise exception.InvalidPortRange(
                         from_port, to_port,
-                        reason="for icmp protocol from_port and to_port must be -1")
+                        reason="for icmp protocol from_port "
+                        "and to_port must be -1")
             else:
                 if from_port < 0 or to_port < 0:
                     raise exception.InvalidPortRange(
