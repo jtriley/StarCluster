1ccce86f7d1d77d3e3da159ecef4811d0157ce58
Ryan <ngsbioinformatics@gmail.com>
2015-04-04 23:32:17 -0400

    Updated log output
    If node doesn't have a DNS name (such as in a VPC) or public IP, the private ip address is all the is available.

--- starcluster/cluster.py
+++ starcluster/cluster.py
@@ -1665,7 +1665,7 @@ class Cluster(object):
         Runs the default StarCluster setup routines followed by any additional
         plugin setup routines. Does not wait for nodes to come up.
         """
-        log.info("The master node is %s" % self.master_node.dns_name)
+        log.info("The master node is %s" % self.master_node.private_id_address)
         log.info("Configuring cluster...")
         if self.volumes:
             self.attach_volumes_to_master()
