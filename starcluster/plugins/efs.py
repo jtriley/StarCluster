c5ae9b239a1ccec7c2c025a893e8ec725f3e9b37
cariaso <cariaso@gmail.com>
2015-11-16 05:38:19 +0000

    clean up enough that it no longer blocks terminate due to the efs security group association

--- starcluster/plugins/efs.py
+++ starcluster/plugins/efs.py
@@ -18,6 +18,7 @@
 from starcluster import clustersetup
 from starcluster.logger import log
 import boto3
+import botocore.exceptions
 
 class EFSPlugin(clustersetup.DefaultClusterSetup):
     """Add EFS support
@@ -66,11 +67,83 @@ class EFSPlugin(clustersetup.DefaultClusterSetup):
         self._volumes = volumes
         log.info("No need to remove %s from EFS" % node.alias)
 
-    def _authorize_efs(self, master):
+    def on_shutdown(self, nodes, master, user, user_shell, volumes):
+        """
+        This method gets executed before shutting down the cluster
+        """
+        self._nodes = nodes
+        self._master = master
+        self._user = user
+        self._user_shell = user_shell
+        self._volumes = volumes
+
+        # I'd like to modify_mount_target_security_groups() with an
+        # empty list, but that doesn't seem to be allowed.  instead
+        # we'll associate with one of the default groups.  however
+        # there will probably be more than one, and I don't have the
+        # VPC ID handy to pick the right one. So I'll get all of the
+        # default groups here, and try each and stop when we find one
+        # that works.
+
+        default_security_groups = master.ec2.get_security_groups(filters={'group-name': 'default'})
+
+        # this is dependent on the config file using a dns, instead of
+        # an IP. a future version should be robust for both forms
         parts = self.dns_name.split('.')
         filesystem = parts[1]
 
         b3client = boto3.client('efs')
+
+        # we need a MountTargetId later, use this to determine it.
+        # notice this is copy and pasted with the same logic found in
+        # _authorize_efs. It would perhaps be better to break it into
+        # a function to be called from both places.
+
+        mount_target_id = None
+        mtresponse = b3client.describe_mount_targets(FileSystemId=filesystem)
+        for mt in mtresponse.get('MountTargets'):
+            subnetid = mt.get('SubnetId')
+            if subnetid == master.subnet_id:
+                mount_target_id = mt.get('MountTargetId')
+                break
+        else:
+            log.info('correct subnet not found')
+            return False
+
+        # we now have our MountTargetId, try to assign it to each of
+        # the default security groups, and stop when one of them works
+        if mount_target_id:
+            log.info('Deauthorizing EFS security group, by reassociating with default')
+            successfully_reassigned = None
+            for a_default_sg in default_security_groups:
+                try:
+                    b3client.modify_mount_target_security_groups(
+                        MountTargetId=mount_target_id,
+                        SecurityGroups=[a_default_sg.id],
+                    )
+                    successfully_reassigned = True
+                    break
+                except botocore.exceptions.ClientError, e:
+                    # couldn't reassociate, probably in wrong vpc
+                    pass
+            return successfully_reassigned
+        return True
+
+
+
+    def _authorize_efs(self, master):
+
+        # this is dependent on the config file using a dns, instead of
+        # an IP. a future version should be robust for both forms
+        parts = self.dns_name.split('.')
+        filesystem = parts[1]
+
+        b3client = boto3.client('efs')
+
+        # we need a MountTargetId later, use this to determine it.
+        # notice this is copy and pasted with the same logic found in
+        # _authorize_efs. It would perhaps be better to break it into
+        # a function to be called from both places.
 
         mount_target_id = None
         mtresponse = b3client.describe_mount_targets(FileSystemId=filesystem)
@@ -85,7 +158,7 @@ class EFSPlugin(clustersetup.DefaultClusterSetup):
 
         if mount_target_id:
             log.info('Authorizing EFS security group')
-            sgresponse = b3client.modify_mount_target_security_groups(
+            b3client.modify_mount_target_security_groups(
                 MountTargetId=mount_target_id,
                 SecurityGroups=[master.cluster_groups[0].id],
             )
