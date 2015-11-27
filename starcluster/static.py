4725551473a640210f2c3bc79989cb65c1c2962a
Mich <fmlheureux@datacratic.com>
2015-11-27 09:37:11 -0500

    Crash on unknown cluster init config

--- starcluster/static.py
+++ starcluster/static.py
@@ -293,7 +293,6 @@ CLUSTER_SETTINGS = {
     'cluster_size': (int, True, None, None, None),
     'cluster_user': (str, False, 'sgeadmin', None, None),
     'cluster_shell': (str, False, 'bash', AVAILABLE_SHELLS.keys(), None),
-    'subnet_id': (str, False, None, None, None),
     'public_ips': (bool, False, None, None, None),
     'master_image_id': (str, False, None, None, None),
     'master_instance_type': (str, False, None, INSTANCE_TYPES.keys(), None),
