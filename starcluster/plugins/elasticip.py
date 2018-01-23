de076e0e05340362340562e7f6396497fb4fdc02
Daniel Treiman <dtreiman@alivecor.com>
2018-01-23 00:32:08 -0800

    Fixed elasticip setup.

--- starcluster/plugins/elasticip.py
+++ starcluster/plugins/elasticip.py
@@ -2,12 +2,13 @@
 # From Charles Cadieu http://star.mit.edu/cluster/mlarchives/1376.html
 
 
-from starcluster.clustersetup import ClusterSetup 
+from starcluster import clustersetup
 from starcluster.logger import log 
 
 
-class ElasticIPSetup(ClusterSetup): 
-    def __init__(self, elastic_ip): 
+class ElasticIPSetup(clustersetup.DefaultClusterSetup):
+    def __init__(self, elastic_ip):
+        super(ElasticIPSetup, self).__init__()
         self.elastic_ip = elastic_ip 
         log.debug('elastic_ip = %s' % elastic_ip) 
 
